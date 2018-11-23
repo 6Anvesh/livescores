2ea04f47d03008cd4c982f133b23ce6e113f8591
krishnasai511 <krishnasai511@gmail.com>
2018-11-23 18:46:30 +0530

    Developing

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static("./dist/livescoresfront-end"));
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/dist/livescoresfront-end/index.html"));
 });
 
