ac9aa6407fee913e14f856a26685acfd60588ff6
reddyloka <anvesh.reddy@venturepact.com>
2018-11-24 10:23:22 +0530

    news added

--- src/app/site/components/home/home.component.ts
+++ src/app/site/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from "@angular/core";
 import { CricketService } from "../../services/cricket.service";
 import { Router } from "../../../../../node_modules/@angular/router";
+import { FootballService } from "../../services/football.service";
 
 declare var $;
 
@@ -10,15 +11,18 @@ declare var $;
   styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  newsData: any=[];
  
 
 
-  constructor(public router: Router) {
-
+  constructor(public router: Router,private footBallServices:FootballService) {
+this.newsapi();
+this.espnNews();
   }
 
   ngOnInit() {
     $('.ui.dropdown').dropdown();
+  
   }
 
   signupClicked() {
@@ -38,5 +42,21 @@ export class HomeComponent implements OnInit {
     this.router.navigateByUrl('/basketball');
   }
 
+  newsapi(){
+this.footBallServices.bbcNews().subscribe(response => {
+  this.newsData.push(response.articles[0]);
+  this.newsData.push(response.articles[1]);
+  this.newsData.push(response.articles[2]);
+  console.log('bbc',this.newsData);
+});
+  }
+  espnNews(){
+    this.footBallServices.espnNews().subscribe(response => {
+  this.newsData.push(response.articles[0]);
+  this.newsData.push(response.articles[1]);
+  this.newsData.push(response.articles[2]);
+  console.log('espn',this.newsData);
+    });
+      }
+  }
 
-}
